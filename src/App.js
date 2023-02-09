bc7f2d5707009a6938499f5ba720b4dce4f0bf15
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-02-09 15:20:53 +0530

    movies app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import {BrowserRouter} from "react-router-dom";
+
+function App() {
+  return<BrowserRouter>
+    <h1>Movies Search</h1>
+  </BrowserRouter>
+}
+
+export default App;
