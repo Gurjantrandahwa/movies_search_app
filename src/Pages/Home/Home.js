b8ed45cdefbedb9f670fae284e919fd9b925e11d
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-02-17 20:36:50 +0530

    movies app

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -99,6 +99,7 @@ export default function Home() {
                     Actors
                 </label>
                 <Button
+                    variant={"outlined"}
                     onClick={onSearch}
                     onChange={onRadioChange}>
                     Search
